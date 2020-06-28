841e41d81d75b920dec262779c54b74dc65ff1eb
Danny Gilbert <spydercapriani@gmail.com>
2020-06-28 00:21:41 -0500

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.2
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftTask",
+    products: [
+        .library(
+            name: "SwiftTask",
+            targets: ["SwiftTask"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "SwiftTask",
+            dependencies: []
+        ),
+    ]
+)
